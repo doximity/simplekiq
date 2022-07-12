cab5a29474b51aa737304d9f4e8b2ad4c6b96399
John Wilkinson <me@jcwilk.com>
2022-07-12 16:55:19 -0700

    Avoid closing and reopening the toplevel batch
    If we open the batch from outside the batch and add more jobs to it then
    we risk a race condition where the empty tracking job might finish
    before we can add in the step batches and jobs.

--- lib/simplekiq/orchestration_executor.rb
+++ lib/simplekiq/orchestration_executor.rb
@@ -14,30 +14,28 @@ module Simplekiq
 
       orchestration_batch.jobs do
         Simplekiq::BatchTrackerJob.perform_async(job.class.name, orchestration_batch.bid, args)
-      end
 
-      new.run_step(orchestration_batch, workflow, 0)
+        new.run_step(workflow, 0)
+      end
     end
 
-    def run_step(orchestration_batch, workflow, step)
+    def run_step(workflow, step)
       *jobs = workflow.at(step)
       # This will never be empty because Orchestration#serialized_workflow skips inserting
       # a new step for in_parallel if there were no inner jobs specified.
 
       next_step = step + 1
-      orchestration_batch.jobs do
-        step_batch = Sidekiq::Batch.new
-        step_batch.description = "Simplekiq orchestrated step #{next_step}"
-        step_batch.on(
-          "success",
-          self.class,
-          {"orchestration_workflow" => workflow, "step" => next_step}
-        )
+      step_batch = Sidekiq::Batch.new
+      step_batch.description = "Simplekiq orchestrated step #{next_step}"
+      step_batch.on(
+        "success",
+        self.class,
+        {"orchestration_workflow" => workflow, "step" => next_step}
+      )
 
-        step_batch.jobs do
-          jobs.each do |job|
-            Object.const_get(job["klass"]).perform_async(*job["args"])
-          end
+      step_batch.jobs do
+        jobs.each do |job|
+          Object.const_get(job["klass"]).perform_async(*job["args"])
         end
       end
     end
@@ -45,8 +43,9 @@ module Simplekiq
     def on_success(status, options)
       return if options["step"] == options["orchestration_workflow"].length
 
-      orchestration_batch = Sidekiq::Batch.new(status.parent_bid)
-      run_step(orchestration_batch, options["orchestration_workflow"], options["step"])
+      Sidekiq::Batch.new(status.parent_bid).jobs do
+        run_step(options["orchestration_workflow"], options["step"])
+      end
     end
   end
 end
